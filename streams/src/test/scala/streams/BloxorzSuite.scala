63e0edf6bccbf3db59f149d611d311275108373b
TomV <tom@viner.tv>
2013-05-14 00:43:22 +0100

    streams: all tests passing upto L1

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -26,6 +26,19 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  trait Level0 extends SolutionChecker {
+      /* terrain for level 0*/
+
+    val level =
+    """------
+      |--ST--
+      |--oo--
+      |--oo--
+      |------""".stripMargin
+
+    val optsolution = List(Down, Right, Up)
+  }
+
   trait Level1 extends SolutionChecker {
       /* terrain for level 1*/
 
@@ -51,31 +64,102 @@ class BloxorzSuite extends FunSuite {
     new Level1 {
       assert(Block(Pos(1, 2), Pos(1, 2)).isStanding)
       assert(!Block(Pos(1, 2), Pos(1, 3)).isStanding)
+      assert(startBlock.down.isStanding === false)
+      assert(startBlock.down.down.isStanding === true)
     }
   }
 
   test("startPos level 1") {
     new Level1 {
-      assert(startPos == Pos(1,1))
+      assert(startPos === Pos(1,1))
     }
   }
 
   test("startBlock level 1") {
     new Level1 {
-      assert(startBlock.b1 == Pos(1,1))
-      assert(startBlock.b2 == Pos(1,1))
+      assert(startBlock.b1 === Pos(1,1))
+      assert(startBlock.b2 === Pos(1,1))
     }
   }
 
-  // test("optimal solution for level 1") {
-  //   new Level1 {
-  //     assert(solve(solution) == Block(goal, goal))
-  //   }
-  // }
+  test("isLegal level 1") {
+    new Level1 {
+      assert(startBlock.down.isLegal === true)
+      assert(startBlock.down.down.isLegal === false)
+    }
+  }
 
-  // test("optimal solution length for level 1") {
-  //   new Level1 {
-  //     assert(solution.length == optsolution.length)
-  //   }
-  // }
+  test("neighbors level 1") {
+    new Level1 {
+      assert(startBlock.neighbors.toSet ===
+        Set(
+            (Block(Pos(1, -1), Pos(1, 0)), Left),
+            (Block(Pos(1, 2), Pos(1, 3)), Right),
+            (Block(Pos(2, 1), Pos(3, 1)), Down),
+            (Block(Pos(-1, 1), Pos(0, 1)), Up)
+        )
+      )
+    }
+  }
+
+  test("legalNeighbors level 1") {
+    new Level1 {
+      assert(startBlock.legalNeighbors.toSet ===
+        Set(
+            (Block(Pos(1, 2), Pos(1, 3)), Right),
+            (Block(Pos(2, 1), Pos(3, 1)), Down)
+        )
+      )
+    }
+  }
+
+  // solver tests
+
+  test("done level 1") {
+    new Level1 {
+      val wrong_solution = List(Right, Right)
+      assert(done(solve(optsolution)) === true)
+      assert(done(solve(optsolution).up) === false)
+      assert(done(solve(optsolution).up.down) === true)
+      assert(done(solve(wrong_solution)) === false)
+    }
+  }
+
+  test("neighborsWithHistory level 1") {
+    new Level1 {
+      assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet ===
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        )
+      )
+    }
+  }
+
+  test("pathsFromStart level 0") {
+    new Level0 {
+      assert(
+        pathsFromStart.map(_._2).toList
+        ===
+        List(
+          List(),
+          List(Down),
+          List(Right, Down),
+          List(Up, Right, Down)
+        )
+      )
+    }
+  }
+
+  test("optimal solution for level 1") {
+    new Level1 {
+      assert(solve(solution) === Block(goal, goal))
+    }
+  }
+
+  test("optimal solution length for level 1") {
+    new Level1 {
+      assert(solution.length === optsolution.length)
+    }
+  }
 }
